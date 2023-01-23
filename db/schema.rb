6c9ca62f9539915e9a0cf611b9e3639847ffb36c
asuna <ghosteluca@gmail.com>
2023-01-23 15:05:57 +0300

    add model Producer with relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_120437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "producers", force: :cascade do |t|
+    t.string "name"
+    t.integer "experience"
+    t.bigint "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_producers_on_team_id"
   end
 
   create_table "ratings", force: :cascade do |t|
@@ -41,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
     t.index ["movie_id"], name: "index_teams_on_movie_id"
   end
 
+  add_foreign_key "producers", "teams"
   add_foreign_key "ratings", "movies"
   add_foreign_key "teams", "movies"
 end
