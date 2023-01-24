587e5c1b3eb52404cc14c30cdc67881e40a00dd6
Andrew Sharapov <shara49750@gmail.com>
2023-01-24 22:45:05 +0300

    link three tables movie, actor, team

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :movies
   resources :actors
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,25 @@ require 'faker'
 puts "Sup"
 Movie.destroy_all
 Actor.destroy_all
+Team.destroy_all
 
 10.times do 
-  Movie.create(
+  movie = Movie.create(
     release_date:Faker::Number.between(from: 1902, to: 2023),
     genre:Faker::Book.genre,
     title:Faker::Movie.title,
     description:Faker::Movie.quote
   )
 
+  team = Team.create(
+    movie: Movie.find(movie.id)
+  )
+
   5.times do
     actor = Actor.create(
     name:  Faker::Name.name,
-    count_of_films: Faker::Number.between(from: 1, to: 50)
+    count_of_films: Faker::Number.between(from: 1, to: 50),
+    team: Team.find(team.id)
   )
   end
 end
