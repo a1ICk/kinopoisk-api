0fe901773bf8ab07627966699714a60e8684cfd6
Andrew Sharapov <shara49750@gmail.com>
2023-01-24 21:54:44 +0300

    add actor model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :movies
+  resources :actors
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Sup"
+Actor.destroy_all
+
+10.times do 
+
+  5.times do
+    actor = Actor.create(
+    name:  Faker::Name.name,
+    count_of_films: Faker::Number.between(from: 1, to: 50)
+  )
+  end
+end
