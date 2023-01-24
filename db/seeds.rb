87a5f02a7d8ed1568cd2abded3e824e0c96e1006
danik <daniil.pavlovich1@gmail.com>
2023-01-24 21:41:00 +0300

    add Movie faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Movie.destroy_all
+
+100.times do
+  Movie.create(
+    release_date:Faker::Number.between(from: 1902, to: 2023),
+    genre:Faker::Book.genre,
+    title:Faker::Movie.title,
+    description:Faker::Movie.quote
+  )
+end
