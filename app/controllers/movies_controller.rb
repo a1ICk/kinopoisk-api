a2f8bb56fc18bba6159987a41e4c213099c0ddaa
daniele <danila.alkevich.dev@gmail.com>
2023-01-26 20:18:55 +0100

    add orders

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,26 +6,33 @@ class MoviesController < ApplicationController
       if url.split('movies').size != 1
         if url.count('&') > 2
           parameters.each do |parameter|
-            if parameter.key?('sortField')
-              @movies = @movies.movie_order_scope(parameter['sortField'])
-            else
-              if parameter['field'].include?('rating')
-                @movies = @movies.rating_scope(parameter['field'].split('.')[1], parameter['search'])
-              else
-                @movies = @movies.movie_scope(parameter['field'], parameter['search'])
+
+              if !parameter.key?('sortField')
+                if parameter['field'].include?('rating')
+                  @movies = @movies.rating_scope(parameter['field'].split('.')[1], parameter['search'])
+                else
+                  @movies = @movies.movie_scope(parameter['field'], parameter['search'])
+                end
               end
-            end
+
           end
         elsif !url.include?('sort')
           if url.include?('rating')
-            @movies = @movies.rating_scope(parameters['field'].split('.')[1], parameters['search'])
+            @movies = @movies.rating_scope(parameters[0]['field'].split('.')[1], parameters[0]['search'])
           else
-            @movies = @movies.movie_scope(parameters['field'], parameters['search'])
+            @movies = @movies.movie_scope(parameters[0]['field'], parameters[0]['search'])
           end
         end
       else
         @movies = Movie.all
       end
+      if url.include?('sortField')
+        @movies = @movies.movie_order_scope(parameters[-1]['sortField'])
+        if parameters[-1]['sortType'] == '-1'
+          @movies = @movies.reverse_order
+        end
+      end
+
 
     render json: @movies, include: [
       :rating,
@@ -49,9 +56,7 @@ class MoviesController < ApplicationController
       first_search = splitted[i].split('=')[1]
       second_field = splitted[i + 1].split('=')[0]
       second_search = splitted[i + 1].split('=')[1]
-      if !first_field.include?('sortField') && !second_field.include?('sortField')
-        result << { first_field => first_search, second_field => second_search }
-      end
+      result << { first_field => first_search, second_field => second_search }
       i+=2
     end
     result
