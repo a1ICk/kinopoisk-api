7b77f9de133b3555a7f422d8d4594951bad1c1bf
daniele <danila.alkevich.dev@gmail.com>
2023-01-26 09:54:33 +0100

    add multiple scope

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,28 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
-    @movies = if request.original_url.split('movies').length != 1
-                if request.original_url.include?('rating.kp') or request.original_url.include?('rating.imdb')
-                  Movie.rating_scope(params['field'].split('.')[1], params['search'])
-                else
-                  Movie.movie_scope(params['field'], params['search'])
-                end
-              else
-                Movie.all
-              end
+    url = request.original_url
+    parameters = split_url(url)
+      if url.split('movies').size != 1
+        if url.count('&') > 2
+          parameters.each do |parameter|
+            if parameter['field'].include?('rating')
+              @movies = @movies.rating_scope(parameter['field'].split('.')[1], parameter['search'])
+            else
+              @movies = @movies.movie_scope(parameter['field'], parameter['search'])
+            end
+          end
+        else
+          if url.include?('rating.kp') or url.include?('rating.imdb')
+            @movies = Movie.rating_scope(parameters['field'].split('.')[1], parameters['search'])
+          else
+            @movies = Movie.movie_scope(parameters['field'], parameters['search'])
+          end
+        end
+      else
+        @movies = Movie.all
+      end
+
     render json: @movies, include: [
       :rating,
       { team: {
@@ -19,5 +32,22 @@ class MoviesController < ApplicationController
         ]
       } }
     ]
+  end
+
+  private
+  def split_url(url)
+    splitted = url.split(/[\/?&]/)[4..]
+
+    result = []
+    i = 0
+    while i < splitted.size
+      first_field = splitted[i].split('=')[0]
+      first_search = splitted[i].split('=')[1]
+      second_field = splitted[i + 1].split('=')[0]
+      second_search = splitted[i + 1].split('=')[1]
+      result << { first_field => first_search, second_field => second_search }
+      i+=2
+    end
+    result
   end
 end
