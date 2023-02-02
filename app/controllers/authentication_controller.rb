c57910e2b3bda1ba63de0d04c8828331c41fc90d
Andrew Sharapov <shara49750@gmail.com>
2023-02-02 04:31:20 +0300

    fix: jwt token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,9 +1,9 @@
-class AuthenticationController < ApplicationController
-  before_action :authenticate_request
-
+class AuthenticationController < ActionController::API
+  include JsonWebToken
+  
   def login
-    @user = User.find_by_email(params[:email])
-    if @user&.authenticate(params[:password])
+    @user = User.find_by(email: params[:user][:email])
+    if @user&.authenticate(params[:user][:password])
       token = jwt_encode(user_id: @user.id)
       render json: { token: token }, status: :ok
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,6 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) 
   end
 end
