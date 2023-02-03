5f8c7516ebcfb9e153214c9642dc14c26caa2df3
danik <daniil.pavlovich1@gmail.com>
2023-02-04 01:10:18 +0300

    add CRUD for producers

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,7 +1,45 @@
 class ProducersController < ApplicationController
+  before_action only: %i[show create update destroy]
   def index
     @producers = Producer.all
+    render_json_producers(@producers)
+  end
 
-    render json: @producers
+  def create
+    @producers = Producer.new(producers_params)
+    if @producers.save
+      render json: { message: 'Producer successfully created' }, status: :ok
+    else
+      render json: { errors: @producers.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @producers = Producer.find(params[:id])
+    render_json_producers(@producers)
+  end
+
+  def update
+    @producers = Producer.find(params[:id])
+    if @producers.update(producers_params)
+      render_json_producers(@producers)
+    else
+      render json: { errors: @actor.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @producers = Producer.find(params[:id])
+    return unless @producers.destroy
+
+    render json: { message: 'Actor successfully deleted' }, status: :ok
+  end
+
+  def render_json_producers(param)
+    render json: param
+  end
+
+  def producers_params
+    params.require(:producer).permit(:name, :experience, :team_id)
   end
 end
