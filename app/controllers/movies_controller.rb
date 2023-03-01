c022d219bfcd5898676e766b80336f8de655f10a
Ivan <drugon.vanya@bk.ru>
2023-03-01 21:23:16 +0300

    add randomizer for movie, 1 GET = 1 movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -36,6 +36,12 @@ class MoviesController < ApplicationController
     render json: { message: 'Movie successfully deleted' }, status: :ok
   end
 
+  def random
+    @mv = Movie.all
+    @movie = @mv[rand(0..@mv.length)]
+    render json: @movie, each_serializer: MovieSerializer, include:
+  end
+
   private
 
   def show_filtered_movies
