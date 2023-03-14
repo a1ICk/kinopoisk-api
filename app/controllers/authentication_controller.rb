bcb8a55352c7592df996361655b0c906ea7988de
Andrew Sharapov <shara49750@gmail.com>
2023-03-15 00:41:10 +0300

    add mailer to Authentication Controller, fix: letter, and default mail to send

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,6 +4,7 @@ class AuthenticationController < ActionController::API
   def login
     @user = User.find_by(email: params[:user][:email])
     if @user&.authenticate(params[:user][:password])
+      AuthenticationMailer.with(user: @user).user_login.deliver_later
       token = jwt_encode(user_id: @user.id)
       render json: { token: token }, status: :ok
     else
--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,18 +1,11 @@
 class RegistrationMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.registration_mailer.user_created.subject
-  #
   def user_created
     @user = params[:user]
-    @greeting = "Hi"
 
     mail( 
-      from: "support@shara.com",
-      to: "shara49754@gmail.com",
-      subject: "You have signed up for Shara on Ireland"
+      from: "support@kinopoisk-api.com",
+      to: @user.email,
+      subject: "You have successfully sign up to Kinopoisk API"
     )
   end
 end
